324042794cf33a6aa60d0c4b63f593f6c467cbd5
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-13 17:40:23 +0500

    Add MongoDB connection setup for backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });
