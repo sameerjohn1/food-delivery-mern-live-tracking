429eb301eb8b98de1cb148460067fb4f586b6890
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-14 10:33:22 +0500

    Add signIn controller to auth.controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -6,7 +6,7 @@ export const signUp = async (req, res) => {
   try {
     const { fullName, email, password, mobile, role } = req.body;
     const user = await User.findOne({ email });
-    if (!user) {
+    if (user) {
       return res.status(400).json({ msg: "User already exist" });
     }
     if (password.length < 6) {
@@ -41,5 +41,32 @@ export const signUp = async (req, res) => {
     return res.status(201).json(user);
   } catch (error) {
     return res.status(500).json(`signup error ${error}`);
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      return res.status(400).json({ message: "incorrect password" });
+    }
+
+    const token = await genToken(user._id);
+    res.cookie("token", token, {
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(`sign In error ${error}`);
   }
 };
