f4f0754cdf54e7d8375c35ca05219e1e019313de
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-19 15:37:29 +0500

    ...

--- frontend/src/pages/Nav.jsx
+++ frontend/src/pages/Nav.jsx
@@ -30,10 +30,10 @@ const Nav = () => {
     px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible "
     >
       {showSearch && (
-        <div className="w-[90%] h-[70px]  bg-white shadow-xl rounded-lg flex items-center gap-[20px] flex fixed top-[80px] left-[5%] ">
+        <div className="w-[90%] h-[70px]  bg-white shadow-xl rounded-lg flex items-center gap-[20px] flex fixed top-[80px] left-[5%] md:hidden">
           <div className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400">
             <FaLocationDot size={25} className="text-[#ff4d3d]" />
-            <div className="w-[80%] truncate text-gray-600">Lahore</div>
+            <div className="w-[80%] truncate text-gray-600">{city}</div>
           </div>
           <div className="w-[80%] flex items-center gap-[10px]">
             <IoIosSearch size={25} className="text-[#ff4d2d]" />
