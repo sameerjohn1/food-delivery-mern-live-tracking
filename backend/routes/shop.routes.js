8327ad041f3e31c819bac3b2abe8a05cd56e6475
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-20 11:42:13 +0500

    feat(routes): add item routes with authentication and image upload

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { createEditShop } from "../controllers/shop.controller.js";
 import isAuth from "../middlewares/isAuth.js";
-
+import { upload } from "../middlewares/multer.js";
 const shopRouter = express.Router();
 
-shopRouter.get("/create-edit", isAuth, createEditShop);
+shopRouter.get("/create-edit", isAuth, upload.single("image"), createEditShop);
 
 export default shopRouter;
