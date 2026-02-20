e655c4d7404a98cccdbaf965fdb9c45439a716ff
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-02-20 11:35:16 +0500

    feat(item): add editItem controller

--- backend/controllers/item.controller.js
+++ backend/controllers/item.controller.js
@@ -26,3 +26,32 @@ export const addItem = async (req, res) => {
     return res.status(500).json({ message: `add item error ${error}` });
   }
 };
+
+export const editItem = async (req, res) => {
+  try {
+    const itemId = req.params.itemId;
+    const { name, category, foodType, price } = req.body;
+    let image;
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+    }
+
+    const item = await Item.findByIdAndUpdate(
+      itemId,
+      {
+        name,
+        category,
+        foodType,
+        price,
+        image,
+      },
+      { new: true },
+    );
+
+    if (!item) return res.status(400).json({ message: "item not found" });
+
+    return res.status(200).json(item);
+  } catch (error) {
+    return res.status(500).json({ message: `edit item error ${error}` });
+  }
+};
